70200ae97b0fd258013e40f9c9f81a7a17f72899
ictzp <zhaopeng@ict.ac.cn>
2017-09-06 01:56:18 +0800

    python script modified

--- run_cc_mem_cold_pagecache.py
+++ run_cc_mem_cold_pagecache.py
@@ -12,16 +12,16 @@ def get_pid(prog_name):
 
 def run_cc(filename, k):
 	#command = 'systemd-run -p MemoryLimit=' + str(k) + 'G -p LimitNOFILE=40000 --setenv=WorkingDirectory=/home/zhaopeng/graph/GridGraph -t' + ' ./bin/wcc ' + filename + ' ' + str(k)
-	command = 'systemd-run -p MemoryLimit=' + str(k) + 'G -p LimitNOFILE=40000 --setenv=LD_LIBRARY_PATH=/usr/local/lib --setenv=WorkingDirectory=/home/zhaopeng/graph/GG-GraphCached -t' + ' ./bin/wcc ' + str(k-1) + " " + filename
+	command = 'systemd-run -p MemoryLimit=' + str(k) + 'G -p LimitNOFILE=40000 --setenv=LD_LIBRARY_PATH=/usr/local/lib --setenv=WorkingDirectory=/home/zhaopeng/graph/GG-GraphCached -t' + ' ./bin/wcc ' + str(k-1.5) + " " + filename
 	p = subprocess.Popen(command, stdout = subprocess.PIPE, shell = True)
 	pid = get_pid("./bin/wcc")
 	print pid
 	print "monitor starts"
-	moniter = subprocess.Popen(["/home/zhaopeng/graph/GG-GraphCached/tools/io_monitor.sh", pid, "wcc_2G_2_16G_directio_cacheap_1M_lru_withouthint.result"])
+	moniter = subprocess.Popen(["/home/zhaopeng/graph/GG-GraphCached/tools/io_monitor.sh", pid, "wcc_2G_2_16G_directio_cacheap_1M_lookahead_watermark.result"])
 	t = p.stdout.read()
 	moniter.kill()
 	print "monitor stops"
-	f = open("wcc_coldpagecache_2G_2_16G_twitter_directio_cacheap_1M_lru_withouthint.result", "a")
+	f = open("wcc_coldpagecache_2G_2_16G_twitter_directio_cacheap_1M_lookahead_watermark.result", "a")
 	f.write(t)
 	print t
 
@@ -33,7 +33,7 @@ def clear_pagecache():
 	subprocess.Popen(command, shell = True)
 	print 'page cahe cleared.'
 
-size = 2
+size = 2.5
 filename = "/home/zhaopeng/graph/data/twitter-2010.gg"
 #filename = "/home/zhaopeng/graph/data/soc-LiveJournal1.txt"
 for sz in range(0, 16):	
